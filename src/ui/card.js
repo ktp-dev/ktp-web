6e0fa6387c6c2fd13448b2819f7ca7718de87bd7
Michael Wilson <mewil@umich.edu>
2018-11-04 15:56:38 -0500

    Update styles

--- src/ui/card.js
+++ src/ui/card.js
@@ -1,17 +1,11 @@
 import { h } from 'react-hyperscript-helpers';
 import styled from 'styled-components';
 
-import { devices } from '@ktp/theme';
 import { Body, BodyHighlight } from './typography';
 
 const Container = styled.div`
   background-size: cover;
-  flex-flow: column wrap;
-  margin: 20px;
   height: 300px;
-  ${devices.tablet`
-    flex: 50%;
-    `};
 `;
 
 export const Card = ({ img, title, body }) =>
